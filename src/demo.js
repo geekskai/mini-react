d3fbb0eef5a58bfa031ede2f2bdcc3a71ee9dc30
gankai <geeks.kai@gmail.com>
2022-01-16 23:25:55 +0800

    feat: :zap: Gankai's react

--- src/demo.js
+++ src/demo.js
@@ -1,19 +1,19 @@
-/** @jsx Gankai.createElement */
-const Gankai = importFromBelow();
+/** @jsx Didact.createElement */
+const Didact = importFromBelow();
 
 const stories = [
-  { name: "Gankai introduction", url: "http://bit.ly/2pX7HNn" },
+  { name: "Didact introduction", url: "http://bit.ly/2pX7HNn" },
   { name: "Rendering DOM elements ", url: "http://bit.ly/2qCOejH" },
   { name: "Element creation and JSX", url: "http://bit.ly/2qGbw8S" },
   { name: "Instances and reconciliation", url: "http://bit.ly/2q4A746" },
   { name: "Components and state", url: "http://bit.ly/2rE16nh" },
 ];
 
-class App extends Gankai.Component {
+class App extends Didact.Component {
   render() {
     return (
-      <div id="666">
-        <h1>Gankai Stories</h1>
+      <div>
+        <h1>Didact Stories</h1>
         <ul>
           {this.props.stories.map((story) => {
             return <Story name={story.name} url={story.url} />;
@@ -24,7 +24,7 @@ class App extends Gankai.Component {
   }
 }
 
-class Story extends Gankai.Component {
+class Story extends Didact.Component {
   constructor(props) {
     super(props);
     this.state = { likes: Math.ceil(Math.random() * 100) };
@@ -50,67 +50,51 @@ class Story extends Gankai.Component {
   }
 }
 
-Gankai.render(<App stories={stories} />, document.getElementById("root"));
+Didact.render(<App stories={stories} />, document.getElementById("root"));
+
+/** ⬇️⬇️⬇️⬇️⬇️ 🌼Didact🌼 ⬇️⬇️⬇️⬇️⬇️ **/
 
 function importFromBelow() {
   let rootInstance = null;
   const TEXT_ELEMENT = "TEXT_ELEMENT";
 
-  function createElement(type, config, ...children) {
+  function createElement(type, config, ...args) {
     const props = Object.assign({}, config);
-    const hasChildren = children.length > 0;
-    const rawChildren = hasChildren ? [].concat(...children) : [];
+    const hasChildren = args.length > 0;
+    const rawChildren = hasChildren ? [].concat(...args) : [];
     props.children = rawChildren
-      .filter((child) => child != null && child !== false)
-      .map((child) =>
-        child instanceof Object ? child : createTextElement(child)
-      );
-
-    return {
-      type,
-      props,
-    };
+      .filter((c) => c != null && c !== false)
+      .map((c) => (c instanceof Object ? c : createTextElement(c)));
+    return { type, props };
   }
 
-  class Component {
-    constructor(props) {
-      this.props = props;
-      this.state = this.state || {};
-    }
-    setState(partialState) {
-      this.state = Object.assign({}, this.state, partialState);
-      updateInstance(this.__internalInstance);
-    }
+  function createTextElement(value) {
+    return createElement(TEXT_ELEMENT, { nodeValue: value });
   }
 
-  function updateInstance(internalInstance) {
-    const parentDom = internalInstance.dom.parentNode;
-    const element = internalInstance.element;
-    reconcile(parentDom, internalInstance, element);
-  }
-
-  function createPublicInstance(element, internalInstance) {
-    const { type, props } = element;
-    const publicInstance = new type(props);
-    publicInstance.__internalInstance = internalInstance;
-    return publicInstance;
+  function render(element, container) {
+    const prevInstance = rootInstance;
+    const nextInstance = reconcile(container, prevInstance, element);
+    rootInstance = nextInstance;
   }
 
   function reconcile(parentDom, instance, element) {
     if (instance == null) {
+      // Create instance
       const newInstance = instantiate(element);
       parentDom.appendChild(newInstance.dom);
       return newInstance;
     } else if (element == null) {
-      // remove instance
+      // Remove instance
       parentDom.removeChild(instance.dom);
       return null;
     } else if (instance.element.type !== element.type) {
-      // replace instance
+      // Replace instance
       const newInstance = instantiate(element);
       parentDom.replaceChild(newInstance.dom, instance.dom);
+      return newInstance;
     } else if (typeof element.type === "string") {
-      // update instance
+      // Update instance
       updateDomProperties(instance.dom, instance.element.props, element.props);
       instance.childInstances = reconcileChildren(instance, element);
       instance.element = element;
@@ -125,46 +109,34 @@ function importFromBelow() {
         oldChildInstance,
         childElement
       );
-
       instance.dom = childInstance.dom;
       instance.childInstance = childInstance;
       instance.element = element;
-
       return instance;
     }
   }
 
   function reconcileChildren(instance, element) {
-    const { dom: parentDom, childInstances } = instance;
+    const dom = instance.dom;
+    const childInstances = instance.childInstances;
     const nextChildElements = element.props.children || [];
     const newChildInstances = [];
     const count = Math.max(childInstances.length, nextChildElements.length);
-
     for (let i = 0; i < count; i++) {
       const childInstance = childInstances[i];
       const childElement = nextChildElements[i];
-      const newChildInstance = reconcile(
-        parentDom,
-        childInstance,
-        childElement
-      );
+      const newChildInstance = reconcile(dom, childInstance, childElement);
       newChildInstances.push(newChildInstance);
     }
-    return newChildInstances.filter((childInstance) => childInstance != null);
-  }
-
-  function render(element, container) {
-    const prevRootInstance = rootInstance;
-    const nextRootInstance = reconcile(container, prevRootInstance, element);
-    rootInstance = nextRootInstance;
+    return newChildInstances.filter((instance) => instance != null);
   }
 
   function instantiate(element) {
     const { type, props } = element;
-
     const isDomElement = typeof type === "string";
-    // Instantiate DOM element
+
     if (isDomElement) {
+      // Instantiate DOM element
       const isTextElement = type === TEXT_ELEMENT;
       const dom = isTextElement
         ? document.createTextNode("")
@@ -190,7 +162,6 @@ function importFromBelow() {
       const dom = childInstance.dom;
 
       Object.assign(instance, { dom, element, childInstance, publicInstance });
-
       return instance;
     }
   }
@@ -199,7 +170,7 @@ function importFromBelow() {
     const isEvent = (name) => name.startsWith("on");
     const isAttribute = (name) => !isEvent(name) && name != "children";
 
-    // remove event listeners
+    // Remove event listeners
     Object.keys(prevProps)
       .filter(isEvent)
       .forEach((name) => {
@@ -207,19 +178,21 @@ function importFromBelow() {
         dom.removeEventListener(eventType, prevProps[name]);
       });
 
-    // remove attributes
+    // Remove attributes
     Object.keys(prevProps)
       .filter(isAttribute)
-      .forEach((name) => (dom[name] = null));
+      .forEach((name) => {
+        dom[name] = null;
+      });
 
-    // set new attributes
+    // Set attributes
     Object.keys(nextProps)
       .filter(isAttribute)
       .forEach((name) => {
         dom[name] = nextProps[name];
       });
 
-    // add event listeners
+    // Add event listeners
     Object.keys(nextProps)
       .filter(isEvent)
       .forEach((name) => {
@@ -227,9 +200,29 @@ function importFromBelow() {
         dom.addEventListener(eventType, nextProps[name]);
       });
   }
+  function createPublicInstance(element, internalInstance) {
+    const { type, props } = element;
+    const publicInstance = new type(props);
+    publicInstance.__internalInstance = internalInstance;
+    return publicInstance;
+  }
 
-  function createTextElement(text) {
-    return createElement(TEXT_ELEMENT, { nodeValue: text });
+  class Component {
+    constructor(props) {
+      this.props = props;
+      this.state = this.state || {};
+    }
+
+    setState(partialState) {
+      this.state = Object.assign({}, this.state, partialState);
+      updateInstance(this.__internalInstance);
+    }
+  }
+
+  function updateInstance(internalInstance) {
+    const parentDom = internalInstance.dom.parentNode;
+    const element = internalInstance.element;
+    reconcile(parentDom, internalInstance, element);
   }
 
   return {
@@ -238,5 +231,3 @@ function importFromBelow() {
     Component,
   };
 }
-
-// =========================================
