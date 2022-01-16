1cc9d4cfd17faeff7e63deaab9c7a37e600c20ed
gankai <geeks.kai@gmail.com>
2022-01-16 21:21:47 +0800

    feat: :fire: add new code

--- src/index.js
+++ src/index.js
@@ -1,210 +1,114 @@
-function createElement(type, props, ...children) {
-  return {
-    type,
-    props: {
-      ...props,
-      children: children.map((child) =>
-        typeof child === "object" ? child : createTextElement(child)
-      ),
-    },
-  };
+const TEXT_ELEMENT = "TEXT_ELEMENT";
+
+let rootInstance = null;
+
+function reconcile(parentDom, instance, element) {
+  if (instance == null) {
+    const newInstance = instantiate(element);
+    parentDom.appendChild(newInstance.dom);
+    return newInstance;
+  } else if (instance.element.type === element.type) {
+    // update instance
+    updateDomProperties(instance.dom, instance.element.props, element.props);
+    instance.childInstances = reconcileChildren(instance, element);
+    instance.element = element;
+    return instance;
+  } else {
+    // replace instance
+    const newInstance = instantiate(element);
+    parentDom.replaceChild(newInstance.dom, instance.dom);
+  }
 }
 
-function createTextElement(text) {
-  return {
-    type: "TEXT_ELEMENT",
-    props: {
-      nodeValue: text,
-      children: [],
-    },
-  };
+function reconcileChildren(instance, element) {
+  const { dom: parentDom, childInstances } = instance;
+  const nextChildElements = element.props.children || [];
+  const newChildInstances = [];
+  const count = Math.max(childInstances.length, nextChildElements.length);
+
+  for (let i = 0; i < count; i++) {
+    const childInstance = childInstances[i];
+    const childElement = nextChildElements[i];
+    const newChildInstance = reconcile(parentDom, childInstance, childElement);
+    newChildInstances.push(newChildInstance);
+  }
+  return newChildInstances.filter((childInstance) => childInstance != null);
 }
 
-function createDom(fiber) {
-  const dom =
-    fiber.type == "TEXT_ELEMENT"
-      ? document.createTextNode("")
-      : document.createElement(fiber.type);
-  updateDom(dom, {}, fiber.props);
-  return dom;
+function render(element, container) {
+  const prevRootInstance = rootInstance;
+  const nextRootInstance = reconcile(container, prevRootInstance, element);
+  rootInstance = nextRootInstance;
 }
 
-const isEvent = (key) => key.startsWith("on");
-const isProperty = (key) => typeof key !== "children" && !isEvent(key);
-const isNew = (prev, next) => (key) => prev[key] !== next;
-const isGone = (prev, next) => (key) => !(key in next);
+function instantiate(element) {
+  const { type, props } = element;
 
-function updateDom(dom, prevProps, nextProps) {
+  const isTextElement = type === TEXT_ELEMENT;
+  const dom = isTextElement
+    ? document.createTextNode("")
+    : document.createElement(type);
+
+  updateDomProperties(dom, {}, props);
+
+  const childElements = props.children || [];
+  const childInstances = childElements.map(instantiate);
+  const childDoms = childInstances.map((childInstance) => childInstance.dom);
+  childDoms.forEach((childDom) => dom.appendChild(childDom));
+  const instance = { dom, element, childInstances };
+
+  return instance;
+}
+
+function updateDomProperties(dom, prevProps, nextProps) {
+  const isEvent = (name) => name.startsWith("on");
+  const isAttribute = (name) => !isEvent(name) && name != "children";
+
+  // remove event listeners
   Object.keys(prevProps)
     .filter(isEvent)
-    .filter((key) => !(key in nextProps) || isNew(prevProps, nextProps)(key))
     .forEach((name) => {
       const eventType = name.toLowerCase().substring(2);
       dom.removeEventListener(eventType, prevProps[name]);
     });
 
-  // Remove old properties
+  // remove attributes
   Object.keys(prevProps)
-    .filter(isProperty)
-    .filter(isGone(prevProps, nextProps))
-    .forEach((name) => {
-      nextProps[name] = "";
-    });
+    .filter(isAttribute)
+    .forEach((name) => (dom[name] = null));
 
-  // Set new or changed properties
+  // set new attributes
   Object.keys(nextProps)
-    .filter(isProperty)
-    .filter(isNew(prevProps, nextProps))
+    .filter(isAttribute)
     .forEach((name) => {
       dom[name] = nextProps[name];
     });
 
-  // Add event listeners
+  // add event listeners
   Object.keys(nextProps)
     .filter(isEvent)
-    .filter(isNew(prevProps, nextProps))
     .forEach((name) => {
       const eventType = name.toLowerCase().substring(2);
       dom.addEventListener(eventType, nextProps[name]);
     });
 }
 
-function commitRoot() {
-  deletions.forEach(commitWork);
-  commitWork(wipRoot.child);
-  currentRoot = wipRoot;
-  wipRoot = null;
-}
+function createElement(type, config, ...children) {
+  const props = Object.assign({}, config);
+  const hasChildren = children.length > 0;
+  const rawChildren = hasChildren ? [].concat(...children) : [];
+  props.children = rawChildren
+    .filter((child) => child != null && child !== false)
+    .forEach((child) =>
+      child instanceof Object ? child : createTextElement(child)
+    );
 
-function commitWork(fiber) {
-  if (!fiber) return;
-  const domParent = fiber.parent.dom;
-  if (fiber.effectTag === "PLACEMENT" && fiber.dom != null) {
-    domParent.appendChild(fiber.dom);
-  } else if (fiber.effectTag === "UPDATE" && fiber.dom != null) {
-    updateDom(fiber.dom, fiber.alternate.props, fiber.props);
-  } else if (fiber.effectTag === "DELETION") {
-    domParent.removeChild(fiber.dom);
-  }
-  commitWork(fiber.child);
-  commitWork(fiber.sibling);
-}
-
-function render(element, container) {
-  wipRoot = {
-    dom: container,
-    props: {
-      children: [element],
-    },
-    alternate: currentRoot,
+  return {
+    type,
+    props,
   };
-  deletions = [];
-  nextUnitOfWork = wipRoot;
 }
 
-let nextUnitOfWork = null;
-let currentRoot = null;
-let wipRoot = null;
-let deletions = null;
-
-function workLoop(deadline) {
-  let shouldYield = false;
-  while (nextUnitOfWork && !shouldYield) {
-    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
-    shouldYield = deadline.timeRemaining() < 1;
-  }
-  if (!nextUnitOfWork && wipRoot) {
-    commitRoot();
-  }
-  requestIdleCallback(workLoop);
+function createTextElement(text) {
+  return createElement(TEXT_ELEMENT, { nodeValue: text });
 }
-requestIdleCallback(workLoop);
-
-function performUnitOfWork(fiber) {
-  if (!fiber.dom) {
-    fiber.dom = createDom(fiber);
-  }
-  const elements = fiber.props.children;
-  reconcileChildren(fiber, elements);
-
-  if (fiber.child) {
-    return fiber.child;
-  }
-  let nextFiber = fiber;
-  while (nextFiber) {
-    if (nextFiber.sibling) {
-      return nextFiber.sibling;
-    }
-    nextFiber = nextFiber.parent;
-  }
-}
-
-function reconcileChildren(wipFiber, elements) {
-  let index = 0;
-  let oldFiber = wipFiber.alternate && wipFiber.alternate.child;
-  let prevSibling = null;
-
-  while (index < elements.length || oldFiber != null) {
-    const element = elements[index];
-    let newFiber = null;
-    const sameType = oldFiber && element && element.type == oldFiber.type;
-    if (sameType) {
-      newFiber = {
-        type: oldFiber.type,
-        props: element.props,
-        dom: oldFiber.dom,
-        parent: wipFiber,
-        alternate: oldFiber,
-        effectTag: "UPDATE",
-      };
-    }
-    if (element && !sameType) {
-      newFiber = {
-        type: element.type,
-        props: element.props,
-        dom: null,
-        parent: wipFiber,
-        alternate: null,
-        effectTag: "PLACEMENT",
-      };
-    }
-    if (oldFiber && !sameType) {
-      oldFiber.effectTag = "DELETION";
-      deletions.push(oldFiber);
-    }
-    if (oldFiber) {
-      oldFiber = oldFiber.sibling;
-    }
-    if (index === 0) {
-      wipFiber.child = newFiber;
-    } else if (element) {
-      prevSibling.sibling = newFiber;
-    }
-    prevSibling = newFiber;
-    index++;
-  }
-}
-
-const Geeks = {
-  createElement,
-  render,
-};
-
-const updateValue = (e) => {
-  reRender(e.target.value);
-};
-
-/** @jsx Geeks.createElement */
-const container = document.getElementById("root");
-
-const reRender = (value) => {
-  const element = (
-    <div>
-      <input onInput={updateValue} value={value} />
-      <h2>{value}</h2>
-    </div>
-  );
-  Geeks.render(element, container);
-};
-
-reRender("World");
